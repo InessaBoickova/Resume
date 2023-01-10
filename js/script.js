23d17aa3fd75dabb40ef8779f807b03954608f95
Inessa <inessa.boickova@yandex.ru>
2023-01-10 22:29:58 +0300

    add promo section

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      close = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click',() =>{
+    menu.classList.add('active');
+    
+});
+close.addEventListener('click',() =>{
+    menu.classList.remove('active');
+});
+
+const interest = document.querySelectorAll('.skills__ratings-interest'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+interest.forEach((item,i) => {
+    lines[i].style.width = item.innerHTML;
+})
+
+
