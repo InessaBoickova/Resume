ec1cce1e0aef14cea38a0501150c265279c9304e
Inessa <inessa.boickova@yandex.ru>
2023-01-14 18:04:33 +0300

    create slider

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ hamburger.addEventListener('click',() =>{
     menu.classList.add('active');
     
 });
+
 close.addEventListener('click',() =>{
     menu.classList.remove('active');
 });
@@ -17,4 +18,34 @@ interest.forEach((item,i) => {
     lines[i].style.width = item.innerHTML;
 })
 
+
+const slides = document.querySelectorAll('.slider__slide');
+    prev = document.querySelector('.slider__prev'),
+    next = document.querySelector('.slider__next');
+let slideIndex = 1;
+
+const showSlides = (n) => {
+    if(n > slides.length){
+        slideIndex = 1 ;
+    }else if(n < 1){
+        slideIndex = slides.length;
+    }
+
+    slides.forEach(i => i.style.display = 'none');
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+showSlides(slideIndex);
+
+const plusSlider = (n)=>{
+    showSlides(slideIndex += n)
+}
+
+prev.addEventListener('click',()=>{
+    plusSlider(- 1);
+})
+
+next.addEventListener('click',()=>{
+    plusSlider(+ 1);
+})
 
